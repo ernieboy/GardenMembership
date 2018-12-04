20666a3a29ad818fdaf2f5150a0149c41b3a2e92
Ernest Fakudze <ernie@ernieboy.com>
2018-12-04 17:17:27 +0000

    Adds more configuration stuff.

---
+++ GardenMembership.SharedKernel/Settings/ApplicationSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace GardenMembership.SharedKernel.Settings
+{
+    public class ApplicationSettings
+    {
+        public ApplicationSecrets ApplicationSecrets { get; set; }
+    }
+
+    public class ApplicationSecrets
+    {
+        public string ConnectionString { get; set; }
+    }
+}
--- GardenMembership.UI/Startup.cs
+++ GardenMembership.UI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using GardenMembership.Application.CommandHandlers;
 using GardenMembership.Domain.DomainEvents;
 using GardenMembership.Infrastructure.Persistence.Implementations;
+using GardenMembership.SharedKernel.Settings;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -19,6 +20,7 @@ namespace GardenMembership.UI
     public class Startup
     {
         private readonly IConfiguration _configuration;
+        private  ApplicationSecrets _applicationSecrets;
 
         public Startup(IConfiguration configuration)
         {
@@ -28,8 +30,10 @@ namespace GardenMembership.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ApplicationSecrets>(_configuration.GetSection("ApplicationSecrets"));
+
             services.AddDbContextPool<GardenMembershipDbContext>( 
-                options => options.UseMySql(_configuration["ConnectionString"],
+                options => options.UseMySql(_configuration["ApplicationSecrets:ConnectionString"],
                     mySqlOptions =>
                     {
                         mySqlOptions.ServerVersion(new Version(5, 7, 22), ServerType.MySql);
